f079b17df2ae4e802953ce6716948bd3a047dd55
Arijit Chakma <arijitchakma79@gmail.com>
2025-01-18 22:21:58 -0500

    added final code

--- Homeworks/Homework1/stringfun.c
+++ Homeworks/Homework1/stringfun.c
@@ -1,3 +1,11 @@
+
+/*
+Name: Arijit Chakma
+Homework 1: CS283
+Purpose: 
+
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -29,12 +37,14 @@ int setup_buff(char *buff, char *user_str, int len) {
     int space_flag = 1;  // Start as 1 to handle leading spaces
     int actual_length = 0;
 
-    // Skip leading spaces
-    while (*src == ' ' || *src == '\t') src++;
+    // Skips leading spaces
+    while (*src == ' ' || *src == '\t') {
+        src++
+    };
 
     while (*src != '\0') {
         if (*src == ' ' || *src == '\t') {
-            if (!space_flag && *(src + 1) != '\0') {  // Add space only if not at end
+            if (!space_flag && *(src + 1) != '\0') {  // Adds space only if not at end
                 if (count >= len) return -1;
                 *dest = ' ';
                 dest++;
@@ -53,7 +63,7 @@ int setup_buff(char *buff, char *user_str, int len) {
         src++;
     }
 
-    // Fill remainder with dots (ensuring full BUFFER_SZ)
+    // Fills remainder with dots
     while (count < len) {
         *dest = '.';
         dest++;
@@ -69,8 +79,9 @@ void print_buff(char *buff, int len) {
     for (int i = 0; i < len; i++) {
         putchar(*(buff + i));
     }
-    printf("]\n");
+    printf("]\n"); 
 }
+
 
 void usage(char *exename){
     printf("usage: %s [-h|c|r|w|x] \"string\" [other args]\n", exename);
@@ -78,12 +89,13 @@ void usage(char *exename){
 }
 
 int count_words(char *buff, int len, int str_len) {
+    (void)len;
     char *ptr = buff;
     int count = 0;
     int in_word = 0;
 
     for (int i = 0; i < str_len; i++) {
-        if (*ptr != ' ' && *ptr != '.') {  // Valid character
+        if (*ptr != ' ' && *ptr != '.') {  
             if (!in_word) { 
                 count++;  
                 in_word = 1;
@@ -102,6 +114,7 @@ int count_words(char *buff, int len, int str_len) {
 //ADD OTHER HELPER FUNCTIONS HERE FOR OTHER REQUIRED PROGRAM OPTIONS
 
 void reverse_string(char *buff, int len, int str_len) {
+    (void)len;
     char *start = buff;
     char *end = buff + str_len - 1;
     char temp;
@@ -117,6 +130,7 @@ void reverse_string(char *buff, int len, int str_len) {
 
 
 void word_print(char *buff, int len, int str_len) {
+    (void)len;
     printf("Word Print\n----------\n");
 
     char *ptr = buff;
@@ -147,25 +161,27 @@ void word_print(char *buff, int len, int str_len) {
     }
     printf("\nNumber of words returned: %d\n", total_words);
 }
+
+
 void replace_string(char *buff, int len, int str_len, char *find, char *replace) {
     char *ptr;
     int find_len = 0, replace_len = 0;
     
-    // Calculate find length
+    // Calculates find length
     ptr = find;
     while (*ptr != '\0') {
         find_len++;
         ptr++;
     }
     
-    // Calculate replace length
+    // Calculates replace length
     ptr = replace;
     while (*ptr != '\0') {
         replace_len++;
         ptr++;
     }
     
-    // Find substring in buffer
+    // Finds substring in buffer
     char *match_start = NULL;
     ptr = buff;
     
@@ -204,18 +220,18 @@ void replace_string(char *buff, int len, int str_len, char *find, char *replace)
         exit(3);  
     }
 
-    // Move characters manually
+    // Moves characters manually
     char *src = match_start + find_len;
     char *dest = match_start + replace_len;
     int shift = replace_len - find_len;
     
-    if (shift < 0) { // Shift left
+    if (shift < 0) { // Shifts left
         while (src < buff + str_len) {
             *dest = *src;
             dest++;
             src++;
         }
-    } else if (shift > 0) { // Shift right
+    } else if (shift > 0) { // Shifts right
         char *end = buff + str_len - 1;
         char *new_end = buff + new_len - 1;
         
@@ -226,7 +242,7 @@ void replace_string(char *buff, int len, int str_len, char *find, char *replace)
         }
     }
 
-    // Copy replacement string
+    // Copies replacement string
     char *replace_ptr = replace;
     ptr = match_start;
     while (*replace_ptr != '\0') {
@@ -235,7 +251,7 @@ void replace_string(char *buff, int len, int str_len, char *find, char *replace)
         replace_ptr++;
     }
 
-    // Fill remainder with dots
+    // Fills remainder with dots
     while (ptr < buff + len) {
         *ptr = '.';
         ptr++;
@@ -254,11 +270,12 @@ int main(int argc, char *argv[]){
 
     //TODO:  #1. WHY IS THIS SAFE, aka what if arv[1] does not exist?
     //      PLACE A COMMENT BLOCK HERE EXPLAINING
+    
     /*
-    Ans: The if and else statement written below ensures that the program does not access and invalid memory address.
-    If argc < 2 or argv[1] does not exist then accessing argv[1] could lead to segmentation fault due to missing argv[1].
-    The condition (*argv[1] != '-') ensures that the first argument after the program is a valid flag. If the user does not provide it
-    then the program will print the usage message and exit.
+        Ans: The if and else statement written below ensures that the program does not access and invalid memory address.
+        If argc < 2 or argv[1] does not exist then accessing argv[1] could lead to segmentation fault due to missing argv[1].
+        The condition (*argv[1] != '-') ensures that the first argument after the program is a valid flag. If the user does not provide it
+        then the program will print the usage message and exit.
     */
 
     if ((argc < 2) || (*argv[1] != '-')){
@@ -295,13 +312,14 @@ int main(int argc, char *argv[]){
     // CODE GOES HERE FOR #3
 
     buff = (char *)malloc(BUFFER_SZ * sizeof(char));
+
     if (buff == NULL) {
         printf("Memory allocation failed\n");
         exit(2);
     }
 
-
     user_str_len = setup_buff(buff, input_string, BUFFER_SZ);     //see todos
+    
     if (user_str_len == -1) {
         printf("Error: Provided input string is too long\n");
         free(buff);
